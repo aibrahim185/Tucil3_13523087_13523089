360106888723263cdc86041b2d462f7cfe880c90
Grace Evelyn Simon <19623035@mahasiswa.itb.ac.id>
2025-05-18 21:14:17 +0700

    fix: dls helper method

--- src/search/ids/ids.cpp
+++ src/search/ids/ids.cpp
@@ -1,38 +1,52 @@
 #include "ids.hpp"
 
 // helper
-SearchNode dls(SearchNode current_node, const Board& initial_board, int limit, int& nodes_total, set<string>& visited) {
-    nodes_total++;
+SearchNode dls(SearchNode initial_node, const Board& initial_board, int limit, int& nodes_total, set<string>& visited) {
+    stack<SearchNode> s;
+    s.push(initial_node);
 
-    if (Utils::is_exit(initial_board, current_node.pieces)) {
-        return current_node;
-    }
+    while (!s.empty()) {
+        SearchNode current_node = s.top();
+        s.pop();
 
-    // jika batas kedalaman saat ini (current_node.val) telah mencapai limit DLS, berhenti
-    if (current_node.val >= limit) {
-        return SearchNode({}, initial_board, {}, -1);
-    }
+        nodes_total++;
 
-    vector<SearchNode> next = Utils::generate_next(current_node, [](const Board&, const vector<Piece>&, const SearchNode& parent_node) {
-        return parent_node.val + 1;
-    });
+        if (current_node.piece_moved != ' ') {
+            Coordinates new_position = {-1,-1}; 
+            Coordinates original_position = current_node.original_position;
 
-    for (const auto& successor_node : next) {
-        string next_state = Utils::state_to_string(successor_node.pieces);
-        
-        if (visited.find(next_state) == visited.end()) {
-            visited.insert(next_state);
+            for(const auto& p_state : current_node.pieces) {
+                if (p_state.id == current_node.piece_moved) {
+                    new_position = p_state.coordinates;
+                    break;
+                }
+            }
+        }
 
-            SearchNode result_node = dls(successor_node, initial_board, limit, nodes_total, visited);
+        if (Utils::is_exit(initial_board, current_node.pieces)) {
+            return current_node;
+        }
+
+        if (current_node.val >= limit) {
+            continue;
+        }
+
+        vector<SearchNode> next = Utils::generate_next(current_node, [](const Board&, const vector<Piece>&, const SearchNode& parent_node) {
+            return parent_node.val + 1;
+        });
+
+        reverse(next.begin(), next.end());
+
+        for (const auto& next_node : next) {
+            string next_state = Utils::state_to_string(next_node.pieces);
             
-            visited.erase(next_state); // backtrack
-
-            if (result_node.val != -1) {
-                return result_node;
+            if (visited.find(next_state) == visited.end()) {
+                visited.insert(next_state);
+                s.push(next_node);
             }
         }
     }
-    return SearchNode({}, initial_board, {}, -1); // tidak ada solusi ditemukan
+    return SearchNode({}, initial_board, {}, -1);
 }
 
 Solution ids::search_ids(const Board& initial_board, const vector<Piece>& initial_pieces) {
@@ -40,7 +54,7 @@ Solution ids::search_ids(const Board& initial_board, const vector<Piece>& initia
     Solution result;
     result.node = 0;
 
-    const int MAX_DEPTH_LIMIT = 100;
+    const int MAX_DEPTH_LIMIT = 100000;
 
     for (int depth_limit = 0; depth_limit <= MAX_DEPTH_LIMIT; ++depth_limit) {        
         SearchNode initial_node(initial_pieces, initial_board, {}, 0);
